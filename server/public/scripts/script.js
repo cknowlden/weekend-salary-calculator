777863e3055587cdcd11268c973d68246978bf8f
Cali Knowlden <calbearnc@gmail.com>
2023-12-11 15:44:36 -0600

    debugged and got js running smoothly now

--- server/public/scripts/script.js
+++ server/public/scripts/script.js
@@ -1,19 +1,14 @@
 console.log('employee salary calc');
 
-function handleSubmit(eventObj){
+function handleSubmit(event){
 	//prevents default action, like a refresh on submit
-	eventObj.preventDefault();
-    // const firstNameInputElement = eventObj.target.querySelector('#firstNameInput');
-    // const lastNameInputElement = eventObj.target.querySelector('#lastNameInput');
-    // const idInputElement = eventObj.target.querySelector('#idInput');
-    // const titleInputElement = eventObj.target.querySelector('#titleInput');
-    // const annualSalaryInputElement = eventObj.target.querySelector('#annualSalaryInput');
+	event.preventDefault();
     const firstNameInputElement = document.getElementById('firstNameInput');
     const lastNameInputElement = document.getElementById('lastNameInput');
     const idInputElement = document.getElementById('idInput');
     const titleInputElement = document.getElementById('titleInput');
     const annualSalaryInputElement = document.getElementById('annualSalaryInput');
-	console.log('Submit', eventObj);
+	console.log('Submit', event);
 	
 	console.log('First name:', firstNameInputElement.value);
 	console.log('Last name:', lastNameInputElement.value);
@@ -25,20 +20,19 @@ function handleSubmit(eventObj){
 	console.log('TABLE:', tableElement)
 	tableElement.innerHTML += `
 		<tr>
-			//updates the DOM
-			<td>${firstNameInputElement.value}</td>
-			<td>${lastNameInputElement.value}</td>
-         <td>${idInputElement.value}</td>
-         <td>${titleInputElement.value}</td>
-         <td>${annualSalaryInputElement.value}</td>
+		    <td>${firstNameInputElement.value}</td>
+		    <td>${lastNameInputElement.value}</td>
+            <td>${idInputElement.value}</td>
+            <td>${titleInputElement.value}</td>
+            <td>${annualSalaryInputElement.value}</td>
 		</tr>
 		`
 		//clear form fields
-		// firstNameInputElement.value = '';
-		// lastNameInputElement.value = '';
-        // idInputElement.value = '';
-        // titleInputElement.value = '';
-        // annualSalaryInputElement.value = '';
+		firstNameInputElement.value = '';
+		lastNameInputElement.value = '';
+        idInputElement.value = '';
+        titleInputElement.value = '';
+        annualSalaryInputElement.value = '';
 }
 
 //module.exports = sayNames;
