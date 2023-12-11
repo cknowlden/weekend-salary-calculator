fa65506cf33b268802fbca60e435b8a9de327b11
Cali Knowlden <calbearnc@gmail.com>
2023-12-11 15:28:38 -0600

    messed up something and now I'm getting errors when I submit

--- server/public/scripts/script.js
+++ server/public/scripts/script.js
@@ -1,34 +1,36 @@
 console.log('employee salary calc');
 
-function handleClick(eventObj){
+function handleSubmit(eventObj){
 	//prevents default action, like a refresh on submit
 	eventObj.preventDefault();
-	
-	//query selector using the eventObj target specifies where to look instead of whole page
-	const firstNameInputElement = eventObj.target.querySelector('#firstNameInput');
-	const lastNameInputElement = eventObj.target.querySelector('#lastNameInput');
-    const idInputElement = eventObj.target.querySelector('#idInput');
-    const titleInputElement = eventObj.target.querySelector('#titleInput');
-    const annualSalaryInputElement = eventObj.target.querySelector('#annualSalaryInput');
+    // const firstNameInputElement = eventObj.target.querySelector('#firstNameInput');
+    // const lastNameInputElement = eventObj.target.querySelector('#lastNameInput');
+    // const idInputElement = eventObj.target.querySelector('#idInput');
+    // const titleInputElement = eventObj.target.querySelector('#titleInput');
+    // const annualSalaryInputElement = eventObj.target.querySelector('#annualSalaryInput');
+    const firstNameInputElement = document.getElementById('firstNameInput');
+    const lastNameInputElement = document.getElementById('lastNameInput');
+    const idInputElement = document.getElementById('idInput');
+    const titleInputElement = document.getElementById('titleInput');
+    const annualSalaryInputElement = document.getElementById('annualSalaryInput');
 	console.log('Submit', eventObj);
 	
-	//the value makes the code look into the area where the user filled out the form block for that element
 	console.log('First name:', firstNameInputElement.value);
 	console.log('Last name:', lastNameInputElement.value);
     console.log('ID:', idInputElement.value);
     console.log('Title:', titleInputElement.value);
     console.log('Annual salary:', annualSalaryInputElement.value);
 
-	const tableElement = document.querySelector('#nameTable tbody');
+	const tableElement = document.querySelector('#employeeTable tbody');
 	console.log('TABLE:', tableElement)
 	tableElement.innerHTML += `
 		<tr>
 			//updates the DOM
 			<td>${firstNameInputElement.value}</td>
 			<td>${lastNameInputElement.value}</td>
-            <td>${idInputElement.value}</td>
-            <td>${titleInputElement.value}</td>
-            <td>${annualSalaryInputElement.value}</td>
+         <td>${idInputElement.value}</td>
+         <td>${titleInputElement.value}</td>
+         <td>${annualSalaryInputElement.value}</td>
 		</tr>
 		`
 		//clear form fields
